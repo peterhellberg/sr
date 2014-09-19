5f08064904cad64b754e18fb342f7f4bb8e0c37b
Peter Hellberg <peter@c7.se>
2014-09-19 13:56:47 +0200

    Added Program type

---
+++ api.v2/program.go
@@ -0,0 +1,21 @@
+package api
+
+type Program struct {
+	Description          string           `json:"description"`
+	ProgramCategory      *ProgramCategory `json:"programcategory"`
+	Payoff               string           `json:"payoff"`
+	BroadcastInfo        string           `json:"broadcastinfo"`
+	Email                string           `json:"email"`
+	Phone                string           `json:"phone"`
+	ProgramURL           string           `json:"programurl"`
+	ProgramImage         string           `json:"programimage"`
+	ProgramImageTemplate string           `json:"programimagetemplate"`
+	SocialImage          string           `json:"socialimage"`
+	SocialImageTemplate  string           `json:"socialimagetemplate"`
+	Channel              *Channel         `json:"channel"`
+	Archived             bool             `json:"archived"`
+	Hasondemand          bool             `json:"hasondemand"`
+	Haspod               bool             `json:"haspod"`
+	Id                   int              `json:"id"`
+	Name                 string           `json:"name"`
+}
