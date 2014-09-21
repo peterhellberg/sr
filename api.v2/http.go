540750722c1f80687a88961eabcf72ff786d2503
Peter Hellberg <peter@c7.se>
2014-09-21 17:57:47 +0200

    Removed the Get function

--- api.v2/http.go
+++ api.v2/http.go
@@ -11,11 +11,6 @@ const (
 	APIBaseURL = "http://api.sr.se/api/v2"
 )
 
-// Get generates a URL and performs a HTTP GET
-func Get(s string, a ...interface{}) (*http.Response, error) {
-	return http.Get(URL(s, a...))
-}
-
 // URL generates a URL to an API endpoint
 func URL(s string, a ...interface{}) string {
 	return fmt.Sprintf("%s/%s", APIBaseURL, fmt.Sprintf(s, a...))
