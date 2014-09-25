4a2ed3972d430421dd9a775e0a01d4c4a616cfb1
Peter Hellberg <peter@c7.se>
2014-09-25 21:12:42 +0200

    Added ScheduledEpisodeService interface skeleton

--- api.v2/scheduledepisodes.go
+++ api.v2/scheduledepisodes.go
@@ -1,5 +1,15 @@
 package api
 
+// ScheduledEpisodesService communicates with the scheduledepisodes
+// related endpoints in the Sveriges Radio API
+type ScheduledEpisodesService interface {
+}
+
+// scheduledEpisodesService implements ScheduledEpisodesService.
+type scheduledEpisodesService struct {
+	client *Client
+}
+
 type ScheduledEpisode struct {
 	EpisodeID    int      `json:"episodeid"`
 	Title        string   `json:"title"`
