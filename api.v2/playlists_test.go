6493d0cc67b4271b6c4e473de4accbbc2664633f
Peter Hellberg <peter@c7.se>
2014-09-25 22:43:05 +0200

    Initial playlists test

---
+++ api.v2/playlists_test.go
@@ -0,0 +1,16 @@
+package api
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPlaylistsGet(t *testing.T) {
+	c := NewClient(nil)
+
+	playlist, err := c.Playlists.Get(132)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "P1", playlist.Channel.Name)
+}
