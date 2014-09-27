2da74bf948571f06672b64a4b4e65c0192aa1e52
Peter Hellberg <peter@c7.se>
2014-09-27 21:29:34 +0200

    Added GetLatest and ByProgramID

--- episodes.go
+++ episodes.go
@@ -1,17 +1,23 @@
 package sr
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Episode represent a Radio episode
 type Episode struct {
-	ID               int      `json:"id"`
-	Title            string   `json:"title"`
-	Description      string   `json:"description"`
-	URL              string   `json:"url"`
-	Program          *Program `json:"program"`
-	PublishDateUTC   string   `json:"publishdateutc"`
-	ImageURL         string   `json:"imageurl"`
-	ImageURLTemplate string   `json:"imageurltemplate"`
-	ListenPodfile    *Podfile `json:"listenpodfile"`
-	DownloadPodfile  *Podfile `json:"downloadpodfile"`
+	ID               int        `json:"id"`
+	Title            string     `json:"title"`
+	Description      string     `json:"description"`
+	URL              string     `json:"url"`
+	Program          *Program   `json:"program"`
+	PublishDateUTC   string     `json:"publishdateutc"`
+	ImageURL         string     `json:"imageurl"`
+	ImageURLTemplate string     `json:"imageurltemplate"`
+	Broadcast        *Broadcast `json:"broadcast"`
+	ListenPodfile    *Podfile   `json:"listenpodfile"`
+	DownloadPodfile  *Podfile   `json:"downloadpodfile"`
 }
 
 // Podfile represent a podfile
@@ -30,9 +36,61 @@ type Podfile struct {
 // EpisodesService communicates with the episodes
 // related endpoints in the Sveriges Radio API
 type EpisodesService interface {
+	GetLatest(id int) (*Episode, error)
+	ByProgramID(id int) ([]*Episode, error)
 }
 
 // episodesService implements EpisodesService.
 type episodesService struct {
 	client *Client
+}
+
+func (s *episodesService) GetLatest(id int) (*Episode, error) {
+	path := fmt.Sprintf("episodes/getlatest?programid=%v&format=json", id)
+
+	req, err := s.client.NewRequest(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var value struct {
+		Episode *Episode `json:"episode"`
+	}
+
+	_, err = s.client.Do(req, &value)
+	if err != nil {
+		return nil, err
+	}
+
+	if value.Episode == nil {
+		return &Episode{}, errors.New("missing episode key in JSON")
+	}
+
+	return value.Episode, nil
+}
+
+// ByProgramID retrieves all episodes by program id
+func (s *episodesService) ByProgramID(id int) ([]*Episode, error) {
+	pathStr := "episodes/index?programid=%v&urltemplateid=3&audioquality=hi&pagination=false&format=json"
+	path := fmt.Sprintf(pathStr, id)
+
+	req, err := s.client.NewRequest(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var value struct {
+		Episodes []*Episode `json:"episodes"`
+	}
+
+	_, err = s.client.Do(req, &value)
+	if err != nil {
+		return nil, err
+	}
+
+	if value.Episodes == nil {
+		return []*Episode{}, errors.New("missing episodes key in JSON")
+	}
+
+	return value.Episodes, nil
 }
--- episodes_test.go
+++ episodes_test.go
@@ -1,1 +1,27 @@
 package sr
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEpisodesGetLatest(t *testing.T) {
+	ts, c := testServerAndClientByFixture("episodes_getlatest_3718")
+	defer ts.Close()
+
+	ep, _ := c.Episodes.GetLatest(3718)
+
+	assert.Equal(t, "PR, opium och ministerdebatt - live från Örebro", ep.Title)
+	assert.Equal(t, 3240, ep.Broadcast.Playlist.Duration)
+}
+
+func TestEpisodesByProgramID(t *testing.T) {
+	ts, c := testServerAndClientByFixture("episodes_3718")
+	defer ts.Close()
+
+	eps, _ := c.Episodes.ByProgramID(3718)
+
+	assert.Equal(t, 3, len(eps))
+	assert.Equal(t, "Abortlagarna, trängselskatterna och pundarboende", eps[2].Title)
+}
