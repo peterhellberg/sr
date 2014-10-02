98f6bb0aa0b63266ef85d54beba6a5da3da4fb25
Peter Hellberg <peter@c7.se>
2014-10-02 23:30:13 +0200

    Added IDName type

--- sport.go
+++ sport.go
@@ -10,45 +10,27 @@ type sportService struct {
 	client *Client
 }
 
+// IDName is a struct with the fields ID and Name
+type IDName struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
 // SportBroadcast represents a sport broadcast
 type SportBroadcast struct {
-	ID             int    `json:"id"`
-	Name           string `json:"name"`
-	Description    string `json:"description"`
-	LocalStartTime string `json:"localstarttime"`
-	LocalStopTime  string `json:"localstoptime"`
-	Hometeam       struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	} `json:"hometeam"`
-	Awayteam struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	} `json:"awayteam"`
-	League struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	} `json:"league"`
-	Season struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	} `json:"season"`
-	Arena struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	} `json:"arena"`
-	Sport struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	} `json:"sport"`
-	Publisher struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	} `json:"publisher"`
-	Channel struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	} `json:"channel"`
+	ID              int        `json:"id"`
+	Name            string     `json:"name"`
+	Description     string     `json:"description"`
+	LocalStartTime  string     `json:"localstarttime"`
+	LocalStopTime   string     `json:"localstoptime"`
+	Hometeam        *IDName    `json:"hometeam"`
+	Awayteam        *IDName    `json:"awayteam"`
+	League          *IDName    `json:"league"`
+	Season          *IDName    `json:"season"`
+	Arena           *IDName    `json:"arena"`
+	Sport           *IDName    `json:"sport"`
+	Publisher       *IDName    `json:"publisher"`
+	Channel         *IDName    `json:"channel"`
 	LiveAudio       *LiveAudio `json:"liveaudio"`
 	MobileLiveAudio *LiveAudio `json:"mobileliveaudio"`
 }
