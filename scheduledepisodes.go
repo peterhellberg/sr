e3abfef5bb1ee580c4cb3c11d48210e8718a375b
Peter Hellberg <peter@c7.se>
2014-11-09 13:23:39 +0100

    wcf.Time instead of string for StartTime/EndTime

--- scheduledepisodes.go
+++ scheduledepisodes.go
@@ -1,5 +1,7 @@
 package sr
 
+import "github.com/peterhellberg/sr/wcf"
+
 // ScheduledEpisodesService communicates with the scheduledepisodes
 // related endpoints in the Sveriges Radio API
 type ScheduledEpisodesService interface {
@@ -12,10 +14,10 @@ type scheduledEpisodesService struct {
 
 // ScheduledEpisode represents a scheduled Radio episode
 type ScheduledEpisode struct {
-	EpisodeID    int      `json:"episodeid"`
-	Title        string   `json:"title"`
-	StartTimeUTC string   `json:"starttimeutc"`
-	EndTimeUTC   string   `json:"endtimeutc"`
-	Program      *Program `json:"program"`
-	Channel      *Channel `json:"channel"`
+	EpisodeID int      `json:"episodeid"`
+	Title     string   `json:"title"`
+	StartTime wcf.Time `json:"starttimeutc"`
+	EndTime   wcf.Time `json:"endtimeutc"`
+	Program   *Program `json:"program"`
+	Channel   *Channel `json:"channel"`
 }
